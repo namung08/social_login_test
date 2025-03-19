b3347921c1000ea34b4650872f9dd57978353bf5
namung08 <namung08@gmail.com>
2025-03-20 03:53:06 +0900

    :tada: feat: social_login_lib 테스트 용 프로젝트 시작

---
+++ src/main/java/co/kr/social_login_test/SocialLoginTestApplication.java
@@ -0,0 +1,13 @@
+package co.kr.social_login_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocialLoginTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/co/kr/social_login_test/SocialLoginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.social_login_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
